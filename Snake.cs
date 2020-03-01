15bbaff42a07d4697f44282dfba09ed46977f90a
Tomek <tomasz0zwierzynski@gmail.com>
2020-03-01 10:42:55 +0100

    Initial commit

---
+++ Snake.cs
@@ -0,0 +1,371 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+
+namespace ConsoleApplication1
+{
+    internal class Program
+    {
+        // Główny panel gry
+        // klasa ta trzyma dane na temat
+        // mapy gry
+        public class GamePanel
+        {
+            public String[,] area = new String[60, 10];
+        }
+        // Tworzymy Punkt oraz jego współrzędne  X, Y
+        public class Punkt
+        {
+            
+            public int IleRazyPowstal = 0;
+            public int xx = 10;
+            public int xy = 8;
+            public Point startPoint = new Point(6, 6);
+            public Boolean Jest = true;
+            // Punkty
+            public void NowaPunktacja()
+            {
+                IleRazyPowstal = 0;
+            }
+            // Rysuje punkt do zdobycia w randowmowym miejscu mapy
+            // Za każdym razem gdy powstaje punkt do zdobycia, 
+            // punkt zdobyty się zwiększa
+            public void RysujeKolejnyPunkt()
+            {
+                Random rnd = new Random();
+
+                int poczatek = 0;
+                int koniec = 60;
+                int wylosowana = rnd.Next(poczatek, koniec);
+
+                xx = wylosowana;
+
+                poczatek = 0;
+                koniec = 10;
+                wylosowana = rnd.Next(poczatek, koniec);
+                xy = wylosowana;
+               
+                IleRazyPowstal++;
+
+            }
+            public Boolean Contains(Point punkt2)
+            {
+                return punkt2.X==xx && punkt2.Y==xy;
+            }
+        }
+
+        // Klasa węża
+        public class Snake
+        {
+            /// Konstruktor węża:
+            /// Wykonuje się gdy tworzymy nowego wężą np:
+            /// Snake snake = new Snake();
+            public Boolean OgonRosnij = false;
+            public Snake()
+            {
+                // Dodaje głowę wężowi
+                // Z punktu startowego
+                this.tail.Add(this.startPoint);
+
+                // Dla długości węża "startTileLength"
+                for (int i = 1; i <= this.startTileLenght; i++)
+                {
+                    // Dodaje kolejne części ogona
+                    this.tail.Add(this.getPointBehind(this.tail.Last()));
+                }
+            }
+
+            // Enum z kierunkami ruchu węża
+            public enum directions
+            {
+                Up,
+                Right,
+                Down,
+                Left
+            }
+
+            // Punkty węża
+            public int score = 0;
+
+            // Aktualny kierunek
+            public int direction = 0;
+
+            // Aktualna prędkość węża
+            public float Speed = 500;
+
+            public float PokazywanSzybkosc = 1;
+
+            // Lista punktów (części ogonu węża)
+            public List<Point> tail = new List<Point>();
+
+            // Punkt startowy węża
+            public Point startPoint = new Point(5, 5);
+
+            // Ilość części ogona przy stworzeniu
+            public int startTileLenght = 3;
+
+            // Zwraca punkt (X, Y) pozycji głowy węża
+            public Point getHeadPosition()
+            {
+                return tail.First();
+            }
+
+            // Zwraca pozycje (do tyłu) względem podanej pozycji do punktu
+            // biorąc pod uwagę kierunek węża
+            public Point getPointBehind(Point point)
+            {
+                Point pointBehind;
+                switch (this.direction)
+                {
+                    case (int)(Snake.directions.Up):
+                        pointBehind = new Point(point.X, point.Y + 1);
+                        break;
+
+                    case (int)(Snake.directions.Right):
+                        pointBehind = new Point(point.X - 1, point.Y);
+                        break;
+
+                    case (int)(Snake.directions.Down):
+                        pointBehind = new Point(point.X, point.Y - 1);
+                        break;
+
+                    case (int)(Snake.directions.Left):
+                        pointBehind = new Point(point.X + 1, point.Y);
+                        break;
+
+                    default:
+                        pointBehind = new Point(-1, -1);
+                        break;
+                }
+
+                return pointBehind;
+            }
+
+            // Zwraca pozycje (do przodu) względem podanej pozycji do punktu
+            // biorąc pod uwagę kierunek węża
+            public Point getPointForward(Point point)
+            {
+                Point pointBehind;
+                switch (this.direction)
+                {
+                    case (int)(Snake.directions.Up):
+                        pointBehind = new Point(point.X, point.Y - 1);
+                        break;
+
+                    case (int)(Snake.directions.Right):
+                        pointBehind = new Point(point.X + 1, point.Y);
+                        break;
+
+                    case (int)(Snake.directions.Down):
+                        pointBehind = new Point(point.X, point.Y + 1);
+                        break;
+
+                    case (int)(Snake.directions.Left):
+                        pointBehind = new Point(point.X - 1, point.Y);
+                        break;
+
+                    default:
+                        pointBehind = new Point(-1, -1);
+                        break;
+                }
+
+                return pointBehind;
+            }
+
+            // Sprawdza czy kolejny ruch węża nie koliduje
+            // ze ścianami lub z samym wężem
+            public Boolean isSafeMove(GamePanel panel)
+            {
+                // Sprawdza pozycję kolejnego ruchu węża
+                Point nextStep = this.getPointForward(this.getHeadPosition());
+
+                // Sprawdza czy ta pozycja nie koliduje z wężem
+                if (this.tail.Contains(nextStep))
+                {
+                    return false;
+                }
+
+                // Sprawdza czy wąż nie wyjedzie za obszar mapy
+                if ((nextStep.X > 59 || nextStep.X < 0) || (nextStep.Y > 9 || nextStep.Y < 0))
+                {
+                    return false;
+                }
+
+                return true;
+            }
+
+            // Rusza węża zależnie od jego kierunku, zwiększa gdy minie punkt
+            public void move()
+            {
+                this.tail.Insert(0, this.getPointForward(this.tail.First()));
+                if (OgonRosnij)
+                {
+                    OgonRosnij = false;
+                }
+                else
+                {
+                    this.tail.Remove(this.tail.Last());
+                }
+            }
+        }
+
+        // Odświeża ekran
+        // czyści konsolę następnie nanosi na nią
+        // pozycję z panelu gry (GamePanel)
+        public static void refreshScreen(GamePanel gamePanel)
+        {
+            Console.Clear();
+            StringBuilder stringBuilder = new StringBuilder();
+
+            for (int y = 0; y < 10; y++)
+            {
+                for (int x = 0; x < 60; x++)
+                {
+                    stringBuilder.Append(gamePanel.area[x, y]);
+
+                    if (x == 59)
+                    {
+                        stringBuilder.Append(Environment.NewLine);
+                    }
+                }
+            }
+
+            Console.WriteLine(stringBuilder);
+        }
+
+        // Generuje mapęna podstawie pozycji węża
+        public static void initializeMap(GamePanel panel, Snake snake, Punkt punkt)
+        {
+            for (int y = 0; y < 10; y++)
+            {
+                for (int x = 0; x < 60; x++)
+                {
+                    if (snake.tail.Contains(new Point(x, y)))
+                    {
+                        panel.area[x, y] = "O";
+                    }
+                    else if (punkt.Contains(new Point(x, y)) && punkt.Jest)
+                    {
+                        panel.area[x, y] = "X";
+                    }
+                    else
+                    {
+                        panel.area[x, y] = "-";
+                    }
+                }
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            GamePanel panel = new GamePanel();
+            Snake snake = new Snake();
+            Punkt punkt = new Punkt();
+            DateTime tijd = DateTime.Now;
+            DateTime tijd2 = DateTime.Now;
+            Boolean buttonPressed = false;
+            Boolean gameOver = false;
+            initializeMap(panel, snake, punkt);
+
+            while (true)
+            {
+                // Jeśli gameOver
+                // czyści konsolę
+                // czeka na input gracza
+                // gdy znajdzie input
+                // resetuje pozycję węża
+                // oraz punktację
+                if (gameOver)
+                {
+
+                    Console.WriteLine("Game Over!!");
+                    Console.ReadKey();
+                    gameOver = false;
+                    snake = new Snake();
+                    punkt.NowaPunktacja();
+                }
+
+                // Odświeża grę
+                // Wypisuje zdibyte punkty oraz prędkość węża
+                tijd = DateTime.Now;
+                buttonPressed = false;
+                initializeMap(panel, snake, punkt);
+                refreshScreen(panel);
+                Console.WriteLine("Zdobyte Punkty {0}", punkt.IleRazyPowstal);
+                Console.WriteLine("Prędkość Węża {0}", snake.PokazywanSzybkosc);
+
+                while (true)
+                {
+                    
+                    tijd2 = DateTime.Now;
+                    if (tijd2.Subtract(tijd).TotalMilliseconds > snake.Speed)
+                    {
+                        break;
+                    }
+
+                    // Sprawdza ktory klawisz jest wcisniety
+                    // i czy klawisz nie jest przeciwny do kierunku ruchu
+                    // węża
+                    if (Console.KeyAvailable)
+                    {
+                        ConsoleKeyInfo pressedKey = Console.ReadKey(true);
+
+                        if (pressedKey.Key.Equals(ConsoleKey.UpArrow) &&
+                            snake.direction != (int)Snake.directions.Down && buttonPressed == false)
+                        {
+                            snake.direction = (int)Snake.directions.Up;
+                            buttonPressed = true;
+                        }
+
+                        if (pressedKey.Key.Equals(ConsoleKey.DownArrow) &&
+                            snake.direction != (int)Snake.directions.Up && buttonPressed == false)
+                        {
+                            snake.direction = (int)Snake.directions.Down;
+                            buttonPressed = true;
+                        }
+
+                        if (pressedKey.Key.Equals(ConsoleKey.LeftArrow) &&
+                            snake.direction != (int)Snake.directions.Right && buttonPressed == false)
+                        {
+                            snake.direction = (int)Snake.directions.Left;
+                            buttonPressed = true;
+                        }
+
+                        if (pressedKey.Key.Equals(ConsoleKey.RightArrow) &&
+                            snake.direction != (int)Snake.directions.Left && buttonPressed == false)
+                        {
+                            snake.direction = (int)Snake.directions.Right;
+                            buttonPressed = true;
+                        }
+                    }
+                }
+
+                // Gdy wąż może się bezpiecznie poruszyć
+                // rusza węża
+                // a jeśli koliduje z czymś (patrz isSafeMove())
+                // GameOver
+                if (snake.isSafeMove(panel))
+                {
+                    snake.move();
+                    // Sprawdza czy zdobyl punkt
+                    Point pun = snake.getHeadPosition();
+                    // Zmienia predkosc, rosnie, rysuje kolejny punkt gdy zdobyl punkt 
+                    if (pun.X == punkt.xx && pun.Y == punkt.xy) 
+                    {
+                        snake.OgonRosnij = true;
+                        punkt.RysujeKolejnyPunkt();
+                        snake.Speed /= (float )1.1;
+                        snake.PokazywanSzybkosc *= (float)1.1;
+                    }
+                }
+                else
+                {
+                    gameOver = true;
+                }
+
+            }
+        }
+    }
+}
---
+++ docs/README.md
@@ -0,0 +1,105 @@
+﻿# Wprowadzenie
+Aplikacja Snake jest to konsolowa gra typu wąż, w której celem gracza jest uzyskanie najdłuższego węża, co wiąże się ze stopniowym wzrostem poziomu trudności gry. Gra się kończy w momencie ugryzienia własnego ogona lub uderzenia w krawędź planszy. Długość węża zwiększana jest za każdym razem, gdy gracz zdobędzie pożywienie. Aby gra nabierała coraz większej dynamiki - za każdym razem, gdy wąż sterowany przez gracza się wydłuża - zwiększa się także prędkość poruszania o 10%. Gracz steruje kierunkiem poruszania się węża za pomocą klawiszy strzałek na klawiaturze.
+
+# Architektura
+
+Aplikacja została napisana w języku C# z wykorzystaniem oprogramowania Visual Studio i wykorzystyje niektóre klasy z biblioteki standardowej C# takie jak:
+- System.Collections - do obsługi list
+- System.Drawing - do geometrii 2D
+- System.Linq - do zapytań na listach
+
+W kodzie źródłowym zdefiniowane są klasy pomocnicze obsługujące różne aspekty gry takie jak rysowanie planszy, obsługa przyrostu ogona węża czy zliczanie zdobytych punktów.
+
+Aplikacja zawiera główną pętlę gry, która w każdym kroku pobiera wejście od użytkownika (naciśnięcia klawiatury), oblicza ruch węża oraz wyświetla zaktualizowany stan gry na konsoli.
+
+# Klasy i metody
+
+## Klasa Punkt
+Klasa Punkt reprezentuje zdobywanie pożywienia, a co za tym idzie, ilość punktów zdobytych przez gracza. Pole *startPoint* inicjalizowane jest punktem o współrzędnych (6,6).
+
+### Pola
+    int IleRazyPowstal;     // jak dużo razy było generowane nowe pożywienie
+    int xx;                 // współrzędna x
+    int xy;                 // współrzędna y
+    Point startPoint;       // punkt startowy
+    Boolean Jest;           // określa, czy istnieje pożywienie na planszy
+
+### Metody
+    void NowaPunktacja();       // resetuje punktacje (IleRazyPowstal)
+    void RysujeKolejnyPunkt();  // losuje współrzędne kolejnego pożywienia na mapie
+    Boolean Contains(Point p);  // sprawdza, czy podany punkt ma takie same współrzędne jak pożywienie
+
+## Klasa GamePanel
+Klasa reprezentuje planszę do gry w węża. Pole *area* inicjalizowane jest prostokątną planszą 60 na 10. Puste miejsca na planszy są reprezentowane przez znak '-', pożywnienie jest reprezentowane przez znak 'X', a ogon węża poprzez znak 'O'.
+
+### Pola
+    String[,] area;     // 2D tablica łańcuchów znaków 
+
+## Klasa Snake
+Klasa reprezentuje ogon sterowanego przez gracza węża oraz jego ruch. Pole *tail* inicjalizowane jest pustą tablicą punktów, pole *Speed* wartością 500, pole *startPoint* punktem o współrzędnych (5,5), a pole *startTileLenght* wartością 3. Wąż podczas ruchu realizowanego przez metodę *move*() ma modyfikowaną listę *tail* tak, że ostatni element listy (ogon) jest usuwany, a do przodu listy dodawany jest nowy element w kolejnym miejscu w kierunku ruchu węża. Wydłużanie ogona węża jest zrealizowane w ten sposób, że gdy zjadanie jest pożywienie, pomijany jest krok usuwania ostatniego elementu listy podczas ruchu węża.
+
+### Definicje wewnętrzne
+    enum Directions     // definicja możliwych kierunków na planszy
+            { 
+                Up, Right, Down, Left
+            }
+
+### Pola
+    Boolean OgonRosnij;         // flaga rośnięcia
+    int score;                  // ilość punktów
+    Directions direction;       // kierunek poruszania się w następnym kroku
+    float Speed;                // prędkość poruszania się
+    float PokazywanSzybkosc;    // prędkość wyświetlana na interfejsie
+    List<Point> tail;           // lista punktów należączych do ogona węża
+    Point startPoint;           // punkt startowy - głowa węża
+    int startTileLenght;        // startowa długość ogonu
+
+### Metody
+    Point getHeadPosition();            // zwraca położenie głowy węża 
+    Point getPointBehind(Point p);      // zwraca punkt poprzedni ogona względem argumentu
+    Point getPointForward(Point p);     // zwraca punkt następny ogona względem argumentu
+    Boolean isSafeMove(GamePanel gp);   // sprawdza, czy ruch może być wykonany (brak przeszkody lub ogona)
+    void move();                        // wykonuje ruch
+
+## Klasa Program
+Klasa Program jest główną klasą programu zawierającą metodę Main będącą punktem wejściowym do aplikacji. Oprócz tego, klasa ta zawiera pomocnicze metody statyczne.
+
+### Metody statyczne
+    void initializeMap(GamePanel panel, Snake snake, Punkt punkt);
+    void refreshScreen(GamePanel gamePanel);
+    void Main(string[] args);
+
+#### initializeMap
+Metoda inicjalizuje planszę gry uzupełniając wszystkie elementy tablicy *area* w obiekcie *panel* o odpowiednie oznaczenia ( 'O', 'X' ub '-' ). Domyślnie pola na planszy są oznaczane przez '-', chyba że dany punkt należy do ogona węża, co jest sprawdzane za pomocą metody snake.tail.Contains(Point p) lub dany punkt jest pożywnieniem, co jest weryfikowane za pomocą koniunkcji wartości zwracanej przez metodę punkt.Contains(Point p) i flagi punkt.Jest.
+
+#### refreshScreen
+Metoda czyści konsolę, a następnie wysyła na nią przygotowany za pomocą StringBuildera łańcuch znaków reprezentujący planszę gry na podstawie obiektu GamePanel przekazanego do metody zawierającego informację o stanie gry.
+
+#### Main
+Metoda implementuje algorytm działania gry. Najpierw tworzone są wszystkie potrzebne obiekty klas: GamePanel, Snake oraz Punkt, następnie inicjalizowane są flagi naciśnięcia przycisku oraz końca gry i inicjalizowana jest plansza gry za pomocą metody statycznej initializeMap(GamePanel panel, Snake snake, Punkt punkt). Kolejnym krokiem jest główna nieskończona pętla, której wykonywanie zostaje rozpoczęte.
+
+Na samym początku tej nieskończonej pętli zostaje sprawdzona flaga końca gry, która jeśli zostanie wykryta, resetuje obiekt węża oraz punktacje. Wyświetlony jest także napis "Game Over" na ekranie. Następnie odświeżana jest mapa oraz wyświetlane są zdobyte punkty i prędkość węża.
+
+Kolejnym krokiem jest oczekiwanie na naciśnięcie przez gracza przycisku sterowania. W zależności od prędkości węża czas ten jest coraz krótszy. Dodatkowo sprawdzane jest czy wciśnięty przez gracza klawisz nie jest przeciwny do kierunku ruchu węża, ponieważ taka zmiana ruchu nie jest możliwa.
+
+Ostatnim krokiem jest sprawdzenie, czy następny ruch węża jest możliwy do wykonania (nie ma ściany lub własnego ogona). Jeśli ruch nie jest możliwy - flaga końca gry zostaje ustawiona. W przeciwnym razie następuje ruch i sprawdzane jest czy wąż napotkał pożywienie. Jeśli tak, zwiększa się długość ogona węża oraz zwiększana jest szybkość ruchu węża. 
+
+Kończy się przebieg pętli i zostaje wykonywana kolejna iteracja.
+
+# Gameplay
+
+Po uruchomieniu aplikacji wąż, gra się automatycznie rozpoczyna. Gracz za pomocą klawiszy strzałek może zmieniać kierunek ruchu węża, którego segmenty ogona reprezentowane są przez znak 'O'. Na poniższym zrzucie ekranu przedstawiono wygląd aplikacji po uruchomieniu: 
+
+![widok po rozpoczeciu](docs/img/screen1.jpg "Widok po rozpoczęciu gry")
+
+Gdy gracz wprowadzi węża w jego własny ogon lub w ścianę, gra się zakańcza i wyświetlany jest napis "Game Over". Aplikacja oczekuje naciśnięcia dowolnego przyciusku, co pozwala rozpocząć grę od początku. Na poniższym zrzucie ekranu przedstawiono wygląd aplikacji po uderzeniu w ścianę:
+
+![widok po przegranej](docs/img/screen2.jpg "Widok po przegranej grze")
+
+Zadaniem gracza jest połykanie pożywienia reprezentowanego przez znak 'X', co powoduje zdobywanie punktów. Zadaniem gracza jest uzyskanie jak najdłuższego ogona. Na poniższym zrzucie ekranu przedstawiono wygląd aplikacji po zdobyciu 14 punktów. Należy zwrócić uwagę na znaczny przyrost prędkości poruszania się wężą.
+
+![widok po zdobyciu 14](docs/img/screen3.jpg "Widok po zdobyciu 14 punktów")
+
+# Podsumowanie
+
+Aplikacja Wąż jest to prosta gra napisana w jezyku C#, która powstała w celach edukacyjnych - umożliwiła poznanie podstaw programowania zorientowanego obiektowo, sposobu używania kolekcji takich jak listy oraz wprowadziła do podstawowych instrukcji sterujących typu *if*, *switch* oraz pętli *while*. Jest w pełni grywalna mimo braku grafiki i pozwala na rywalizację między różnymi graczami dzięki pokazywaniu zdobytych punktów.  
